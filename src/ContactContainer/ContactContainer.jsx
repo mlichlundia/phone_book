f1b3f862dc5f34bad6688350e55c4893a04e1c52
Sychova Ksenia <mlichlundiapl@gmail.com>
2021-12-29 21:28:17 +0100

    replace sort from render part

--- src/ContactContainer/ContactContainer.jsx
+++ src/ContactContainer/ContactContainer.jsx
@@ -2,20 +2,24 @@ import React from 'react'
 import Contact from '../Contact/Contact'
 import './ContactContainer.css'
 
+let contacts
+function sortContacts(list) {
+  console.log(list)
+  contacts = list
+  contacts.sort((a, b) => {
+    if (a > b) {
+      return 1
+    } else if (a < b) {
+      return -1
+    } else return 0
+  })
+}
+
 function ContactContainer(props) {
-  function sortContacts() {
-    props.contactList.sort((a, b) => {
-      if (a > b) {
-        return 1
-      } else if (a < b) {
-        return -1
-      } else return 0
-    })
-  }
-  sortContacts()
+  sortContacts(props.contactList)
   return (
     <div className="contact-container">
-      {props.contactList.map((contact) => (
+      {contacts.map((contact) => (
         <Contact key={contact} name={contact} />
       ))}
     </div>
