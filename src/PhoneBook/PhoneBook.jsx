9cf765e49cd12ecff08511e03761d23d98234534
Sychova Ksenia <mlichlundiapl@gmail.com>
2022-01-02 16:08:27 +0100

    add sort method

--- src/PhoneBook/PhoneBook.jsx
+++ src/PhoneBook/PhoneBook.jsx
@@ -3,19 +3,31 @@ import ContactContainer from '../ContactContainer/ContactContainer'
 import data from '../data'
 import './PhoneBook.css'
 
-function PhoneBook() {
-  let obj = {}
-  for (let contact of data) {
-    if (obj.hasOwnProperty(contact[0])) {
-      obj[contact[0]].push(contact)
-    } else {
-      obj[contact[0]] = [contact]
-    }
+let obj = {}
+for (let contact of data) {
+  if (obj.hasOwnProperty(contact[0])) {
+    obj[contact[0]].push(contact)
+  } else {
+    obj[contact[0]] = [contact]
   }
+}
 
+function sortContacts(list) {
+  return list.sort((a, b) => {
+    if (a > b) {
+      return 1
+    } else if (a < b) {
+      return -1
+    } else return 0
+  })
+}
+
+let list = sortContacts(Object.values(obj))
+
+function PhoneBook() {
   return (
     <div className="main-container">
-      {Object.values(obj).map((contacts, index) => (
+      {list.map((contacts, index) => (
         <ContactContainer key={index} contactList={contacts} />
       ))}
     </div>
