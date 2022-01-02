9d8be368a89f0456bfe6b1dfcb3da188185213b3
Sychova Ksenia <mlichlundiapl@gmail.com>
2022-01-02 15:58:09 +0100

    delete sort method

--- src/ContactContainer/ContactContainer.jsx
+++ src/ContactContainer/ContactContainer.jsx
@@ -3,16 +3,6 @@ import Contact from '../Contact/Contact'
 import './ContactContainer.css'
 
 function ContactContainer(props) {
-  function sortContacts() {
-    props.contactList.sort((a, b) => {
-      if (a > b) {
-        return 1
-      } else if (a < b) {
-        return -1
-      } else return 0
-    })
-  }
-  sortContacts()
   return (
     <div className="contact-container">
       {props.contactList.map((contact) => (
