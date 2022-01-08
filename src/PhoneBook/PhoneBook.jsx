e2d12f8a59e33eaed6471fe0397f0c4e7d3b53b9
Sychova Ksenia <mlichlundiapl@gmail.com>
2022-01-08 15:26:59 +0100

    ready

--- src/PhoneBook/PhoneBook.jsx
+++ src/PhoneBook/PhoneBook.jsx
@@ -6,22 +6,12 @@ import './PhoneBook.css'
 import SearchBar from '../SearchBar/SearchBar'
 
 let obj = {}
-for (let contact of sortContacts(data)) {
+for (let contact of data.sort()) {
   if (obj.hasOwnProperty(contact[0])) {
     obj[contact[0]].push(contact)
   } else {
     obj[contact[0]] = [contact]
   }
-}
-
-function sortContacts(list) {
-  return list.sort((a, b) => {
-    if (a > b) {
-      return 1
-    } else if (a < b) {
-      return -1
-    } else return 0
-  })
 }
 
 let list = Object.values(obj)
